a58b843399f435aada5bc08964d3ce4930cb0069
André Roque Matheus <amatheus@pensafaz.com.br>
2024-12-03 01:15:04 -0300

    Solve Day2, part 2

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -14,9 +14,9 @@ fn main() {
     let result = day2_part1(&input);
     println!("Day 2 part1: {result}");
 
-    // assert_eq!(0i64, day2_part2(example));
-    // let result = day2_part2(&input);
-    // println!("Day 2 part2: {result}");
+    assert_eq!(4, day2_part2(example));
+    let result = day2_part2(&input);
+    println!("Day 2 part2: {result}");
 }
 
 fn day2_part1(input: impl AsRef<str>) -> usize {
@@ -27,8 +27,12 @@ fn day2_part1(input: impl AsRef<str>) -> usize {
         .count()
 }
 
-fn day2_part2(input: impl AsRef<str>) -> i64 {
-    todo!()
+fn day2_part2(input: impl AsRef<str>) -> usize {
+    let input = parse_input(input);
+    input.iter()
+        .map(|mut level| level.safe_after_dampening())
+        .filter(|b| *b)
+        .count()
 }
 
 fn parse_input(input: impl AsRef<str>) -> Vec<Vec<i64>> {
@@ -44,6 +48,7 @@ trait Level {
     fn is_safe(&self) -> bool;
     fn is_increasing_or_decreasing(&self) -> bool;
     fn adjacents_differ_at_least_at_most(&self, at_least: i64, at_most: i64) -> bool;
+    fn safe_after_dampening(&mut self) -> bool;
 }
 
 impl Level for &Vec<i64> {
@@ -69,6 +74,21 @@ impl Level for &Vec<i64> {
             }
         }
         true
+    }
+
+    fn safe_after_dampening(&mut self) -> bool {
+        if self.is_safe() {
+            true
+        } else {
+            for i in 0..self.len() {
+                let mut v = self.clone();
+                v.remove(i);
+                if (&v).is_safe() {
+                    return true;
+                }
+            }
+            false
+        }
     }
 }
 
